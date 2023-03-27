b4ce163480a166505837a1ddc8c69941d17e9b97
ProJend <projend@outlook.com>
2023-03-27 15:53:18 +0800

    Minimize Bottom Button

--- TrueLove.UWP/Views/CommentsPage.xaml.cs
+++ TrueLove.UWP/Views/CommentsPage.xaml.cs
@@ -25,9 +25,42 @@ namespace TrueLove.UWP.Views
         /// <summary>
         /// 返回顶部按钮。
         /// </summary>
-        private void BackToTop_Click(object sender, RoutedEventArgs e) => Show_ScrollViewer.ChangeView(null, 0, null);
+        private void BackToTop_Click(object sender, RoutedEventArgs e) => hapticScrollViewer.ChangeView(null, 0, null);
 
         private void OnWindowActivated(object sender, WindowActivatedEventArgs e) => VisualStateManager.GoToState(this,
                 e.WindowActivationState == CoreWindowActivationState.Deactivated ? WindowNotFocused.Name : WindowFocused.Name, false);
+
+        /// <summary>
+        /// 检查工具栏相关的按钮可用状态。
+        /// 下滑隐藏工具栏 https://www.cnblogs.com/lonelyxmas/p/9919869.html
+        /// </summary>
+        private void hapticScrollViewer_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
+        {
+            // 滚动条当前位置小于存储的变量值时代表往上滑，即不可以下滑隐藏
+            // 增加额外距离以防误触
+            if (hapticScrollViewer.VerticalOffset > scrlocation + 1 && canMinimize)
+            {
+                ToolBarSlideDownStoryboard.Begin();
+                canMinimize = false;
+            }
+            else if (hapticScrollViewer.VerticalOffset < scrlocation - 18 && !canMinimize)
+            {
+                ToolBarSlideUpStoryboard.Begin();
+                canMinimize = true;
+            }
+            else if (hapticScrollViewer.VerticalOffset == 0 && !canMinimize)
+            {
+                ToolBarSlideUpStoryboard.Begin();
+                canMinimize = true;
+
+            }
+            scrlocation = hapticScrollViewer.VerticalOffset;
+        }
+
+        // 滚动条位置变量
+        double scrlocation = 0;
+
+        // 导航栏当前显示状态（这个是为了减少不必要的开销，因为我做的是动画隐藏显示效果如果不用一个变量来记录当前导航栏状态的会重复执行隐藏或显示）
+        bool canMinimize = true;
     }
 }
