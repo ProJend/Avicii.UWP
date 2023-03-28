337e8083347d8d55082c67a3dfabf2c69ed05e1a
ProJend <projend@outlook.com>
2023-03-28 18:52:58 +0800

    test local html data

---
+++ TrueLove.UWP/Spider/URLRefining.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using Windows.Storage.Search;
+using Windows.UI.Xaml.Controls;
+
+namespace TrueLove.UWP.Spider
+{
+    public class URLRefining
+    {
+        public URLRefining()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path">Use URL address</param>
+        /// <param name="isLoadedDown">switch to use website address or local document address</param>
+        public async void ReadHTML(string path, bool isLoadedDown = true)
+        {
+            if (isLoadedDown)
+            {
+                strHTML = File.ReadAllText(path);
+            }
+            else
+            {
+                var httpClient = new HttpClient();
+                httpClient.DefaultRequestHeaders.ConnectionClose = true;
+                strHTML = await httpClient.GetStringAsync(new Uri(path));
+            }
+        }
+
+        void RefineDate()
+        {
+
+        }
+
+        private string strHTML;
+        internal string StrHTML { get => strHTML; }
+    }
+}
--- TrueLove.UWP/Views/CommentsPage.xaml.cs
+++ TrueLove.UWP/Views/CommentsPage.xaml.cs
@@ -1,8 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
 using TrueLove.Lib.Models.Code;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using TrueLove.UWP.Spider;
+using Windows.ApplicationModel;
 
 // https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
 
@@ -19,9 +23,11 @@ namespace TrueLove.UWP.Views
             this.InitializeComponent();
             Comments = CommentManager.GetComment();
             Window.Current.Activated += OnWindowActivated; // 订阅窗口活动事件
+            var readHTML = new URLRefining();
 
+            readHTML.ReadHTML(Package.Current.InstalledPath + "/Spider/TestCode.txt");
+            aa.Text = readHTML.StrHTML;
         }
-
         /// <summary>
         /// 返回顶部按钮。
         /// </summary>
